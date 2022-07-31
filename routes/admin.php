0649c9df55bd700b6dbc8aa9413a3ccfe0697389
Bảo Nguyễn Văn <nvbluutru@gmail.com>
2022-07-31 18:44:35 +0700

    upload admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\RouteController;
 use App\Http\Controllers\Admin\OfficeController;
 
-Route::post('login', [AdminController::class, 'handleLogin'])->name("handleAdminLogin");
+Route::post('login', [AdminController::class, 'handleLogin'])->name("handleAdminLoginád");
 
 Route::group(['middleware' => ['guest:admin']], function () {
     Route::get('login', [AdminController::class, 'login'])->name("adminLogin");
